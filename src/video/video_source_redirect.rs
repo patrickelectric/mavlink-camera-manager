962e41da9b0237dc27bb50e3b3cd15ed1643e640
João Antônio Cardoso <joao.maker@gmail.com>
2023-03-30 16:22:00 -0300

    WIP: Onvif

--- src/video/video_source_redirect.rs
+++ src/video/video_source_redirect.rs
@@ -1,12 +1,16 @@
 use super::types::*;
 use super::video_source::{VideoSource, VideoSourceAvailable};
 
+use anyhow::Result;
+use onvif::discovery;
 use paperclip::actix::Apiv2Schema;
 use serde::{Deserialize, Serialize};
+use tracing::*;
 
 #[derive(Apiv2Schema, Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub enum VideoSourceRedirectType {
     Redirect(String),
+    Onvif(url::Url),
 }
 
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
@@ -23,12 +27,13 @@ impl VideoSource for VideoSourceRedirect {
     fn source_string(&self) -> &str {
         match &self.source {
             VideoSourceRedirectType::Redirect(string) => string,
+            VideoSourceRedirectType::Onvif(url) => url.as_str(),
         }
     }
 
     fn formats(&self) -> Vec<Format> {
         match &self.source {
-            VideoSourceRedirectType::Redirect(_) => {
+            VideoSourceRedirectType::Redirect(_) | VideoSourceRedirectType::Onvif(_) => {
                 vec![]
             }
         }
@@ -68,7 +73,7 @@ impl VideoSource for VideoSourceRedirect {
 
     fn is_valid(&self) -> bool {
         match &self.source {
-            VideoSourceRedirectType::Redirect(_) => true,
+            VideoSourceRedirectType::Redirect(_) | VideoSourceRedirectType::Onvif(_) => true,
         }
     }
 
@@ -79,9 +84,59 @@ impl VideoSource for VideoSourceRedirect {
 
 impl VideoSourceAvailable for VideoSourceRedirect {
     fn cameras_available() -> Vec<VideoSourceType> {
-        vec![VideoSourceType::Redirect(VideoSourceRedirect {
+        let mut sources = vec![VideoSourceType::Redirect(VideoSourceRedirect {
             name: "Redirect source".into(),
             source: VideoSourceRedirectType::Redirect("Redirect".into()),
-        })]
+        })];
+
+        // let (tx, mut rx) = tokio::sync::oneshot::channel();
+
+        // std::thread::spawn(move || {
+        //     tokio::task::spawn_blocking(|| async move {
+        //         if let Err(error) = tx.send(discover().await) {
+        //             error!("Failed sending discovered Onvif sources: {error:?}")
+        //         }
+        //     });
+        // });
+        // info!("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+
+        // loop {
+        //     match rx.try_recv() {
+        //         Ok(Ok(onvif_sources)) => {
+        //             sources.append(onvif_sources.as_mut());
+        //             break;
+        //         }
+        //         Ok(Err(error)) => error!("Failed discovering Onvif sources: {error:?}"),
+        //         Err(_) => (),
+        //     }
+        //     std::thread::sleep(std::time::Duration::from_millis(100));
+        // }
+        // info!("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+
+        sources
     }
+}
+
+async fn discover() -> Result<Vec<VideoSourceType>> {
+    use futures::stream::iter;
+    use futures::stream::StreamExt;
+
+    let videos = discovery::DiscoveryBuilder::default()
+        .duration(std::time::Duration::from_secs(5))
+        .listen_address("0.0.0.0".parse().unwrap())
+        .run()
+        .await?
+        .map(|device| {
+            iter(device.urls.into_iter().map(move |url| {
+                VideoSourceType::Redirect(VideoSourceRedirect {
+                    name: device.name.clone().unwrap_or("Unnamed".into()),
+                    source: VideoSourceRedirectType::Onvif(url.clone()),
+                })
+            }))
+        })
+        .flatten()
+        .collect()
+        .await;
+
+    Ok(videos)
 }
